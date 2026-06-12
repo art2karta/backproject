a01a04a0bded9cbc5b6d277ae1ec3ec38e0e0ce5
Arty Kartavtsev <101965810+art15karta@users.noreply.github.com>
2026-06-13 03:47:57 +0700

    Postrge fixed errors

--- services/user-service/app/publisher.py
+++ services/user-service/app/publisher.py
@@ -1,12 +1,24 @@
 import pika
 import json
+import os
 
-RABBITMQ_HOST = "rabbitmq"
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", "admin")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", "admin")
 
 
 def publish_user_created(user_id: int, email: str):
+
+    credentials = pika.PlainCredentials(
+        RABBITMQ_USER,
+        RABBITMQ_PASSWORD
+    )
+
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=RABBITMQ_HOST)
+        pika.ConnectionParameters(
+            host=RABBITMQ_HOST,
+            credentials=credentials
+        )
     )
 
     channel = connection.channel()
--- services/user-service/app/schemas/user.py
+++ services/user-service/app/schemas/user.py
@@ -6,7 +6,7 @@ from pydantic import Field
 class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(
-        min_length=8,
+        min_length=6,
         max_length=100,
     )
 
